6647d1377c7dd798959985a00d96410ffa8868e7
tao.chen <xxx.tao.c@gmail.com>
2024-08-12 23:39:38 +0800

    Code style and add the create_chrome_driver method to improve the efficiency of obtaining data

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import json
 import os
 
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 def save_json(data, filename):
@@ -29,10 +32,21 @@ def download_image(image_url, save_path):
             os.makedirs(os.path.dirname(save_path), exist_ok=True)
             with open(save_path, 'wb') as file:
                 file.write(response.content)
-            return (image_url, True)
+            return image_url, True
         else:
             print(f"Failed to download {image_url}: Status code {response.status_code}")
-            return (image_url, False)
+            return image_url, False
     except requests.RequestException as e:
         print(f"Error downloading {image_url}: {e}")
-        return (image_url, False)
+        return image_url, False
+
+
+def create_chrome_driver():
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+    chrome_options.add_argument('--disable-images')  # Optional: Disable image loading
+    chrome_options.set_capability('pageLoadStrategy', 'eager')  # Eager page load strategy
+    service = Service('native/chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.set_page_load_timeout(30)  # Set page load timeout to 30 seconds
+    return driver
